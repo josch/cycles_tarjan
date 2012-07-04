bd097aef4a3896daa4a5c9bcfc85af091c4a9f02
josch <j.schauer@email.de>
2012-07-04 14:03:48 +0200

    add copyright notice

--- cycles.py
+++ cycles.py
@@ -52,6 +52,13 @@ procedure circuit_enumeration;
   end;
 """
 
+# Copyright (C) 2012 Johannes Schauer <j.schauer@email.de>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
 import sys
 
 if len(sys.argv) < 3:
